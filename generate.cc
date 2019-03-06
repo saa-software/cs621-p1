3f6beb2e606ede117f3eda275b5b550cdb005198
Allison Wong <azywong@gmail.com>
2019-03-05 23:04:49 -0800

    adding generation code for packet data

---
+++ generate.cc
@@ -0,0 +1,30 @@
+#include <fstream>
+using namespace std;
+#include <iostream>
+
+int 
+main (int argc, char *argv[])
+{
+	int *m_data;
+	char *newLine = "\n";
+	ifstream file;
+	file.open ("/dev/random");
+
+	ofstream ofile;
+	ofile.open ("data.txt");
+	
+	if (file.is_open ())
+	{
+	  for (int i = 0; i < 6000; ++i)
+	  {
+	  	m_data = new int [1100];
+	  	file.read((char*)m_data, 1100);
+	  	ofile.write((char*)&m_data, 1100);
+	  	ofile.write(newLine, sizeof(newLine));
+	  	delete [] m_data;
+	  }
+	}
+
+	ofile.close();
+	file.close();
+}
